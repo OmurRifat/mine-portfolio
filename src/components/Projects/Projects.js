98fff877116e7f9ff3fcd1c851480fcf2eb52446
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-12-14 11:40:45 +0600

    Structure added

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <h4>This is Projects section.</h4>
+        </div>
+    );
+};
+
+export default Projects;
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div>
+            <h4>This is skills section.</h4>
+        </div>
+    );
+};
+
+export default Skills;
